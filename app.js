9dff45d362e3cdf2d3f23849ae10f7e77c3b2979
Myangaa <m.myanganbayar@gmail.com>
2019-12-15 22:59:36 +0700

    financeControler-iin function-uudiig bichij ehel

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+  };
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+})();
 
 // Санхүүтэй ажиллах контроллер
-var financeController = (function() {})();
+var financeController = (function() {
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      inc: 0,
+      exp: 0
+    }
+  };
+  
+})();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
@@ -12,16 +55,27 @@ var appController = (function(uiController, financeController) {
     // 3. Олж авсан өгөгдлүүдээ вебийн тохирох хэсэгт гаргана.
     // 4.Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
-    console.log("Enter daragdsan");
+    console.log(uiController.getInput());
   }
 
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    ctrlAddItem();
-  });
+  var setupEventListener = function() {
+    document
+      .querySelector(uiController.getDOMstrings().addBtn)
+      .addEventListener("click", function() {
+        ctrlAddItem();
+      });
 
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  return {
+    init: function() {
+      setupEventListener();
     }
-  });
+  };
 })(uiController, financeController);
+
+appController.init();
